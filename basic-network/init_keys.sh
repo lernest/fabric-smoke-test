a211f2e28462a8a5d4dd9e4dcd689db9875963ac
Liam.Ernest <Liam.Ernest@changehealthcare.com>
2020-05-18 18:38:32 -0700

    files added

---
+++ basic-network/init_keys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#  GitLab prevents any .pem files from being published
+#  However, the keys used in this example are for testing purposes only
+#  and do not contain any personal or otherwise confidential information
+#
+#  To get around this restriction, the .pem extentions are changed to .txt
+#  This can be done by running the `hide_keys.sh` script.
+#
+#  The purpose of this `init_keys.sh` script is to convert these files back
+#  The .txt extensions will be switched back to .pem
+#
+#  Do not use this technique if the .pem files contain any information
+#  that is actually confidential.
+
+mv crypto-config/ordererOrganizations/example.com/ca/ca.example.com-cert.txt crypto-config/ordererOrganizations/example.com/ca/ca.example.com-cert.pem
+mv crypto-config/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.txt crypto-config/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
+mv crypto-config/peerOrganizations/org1.example.com/ca/org1.example.com-cert.txt crypto-config/peerOrganizations/org1.example.com/ca/org1.example.com-cert.pem
